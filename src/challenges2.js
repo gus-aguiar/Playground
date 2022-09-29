987cb41621a29f3828a779daa6d0ca52bd8237f7
gus-aguiar <gustavopma90@gmail.com>
2022-09-29 10:13:24 -0300

    fazendo a onze

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,31 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+  let tel = []
+  if(number.length > 10){
+    return 'Array com tamanho incorreto.'
+  }
+  for(let key in number){
+    if(number[key] > 9)
+  }
+    tel.push('(');
+    tel.push(number[0])
+    tel.push(number[1])
+    tel.push(')')
+    tel.push(' ')
+    tel.push(number[2])
+    tel.push(number[3])
+    tel.push(number[4])
+    tel.push(number[5])
+    tel.push(number[6])
+    tel.push('-')
+    tel.push(number[7])
+    tel.push(number[8])
+    tel.push(number[9])
+    tel.push(number[10])
+
+  return tel.join('')
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
